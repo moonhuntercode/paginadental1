a525631c0357a706debe432aea0388ffd3b22ea5
amaroksc <53928868+amaroksc@users.noreply.github.com>
2021-01-17 23:05:01 -0400

    cambio-index1

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portada </title>
+    <link rel="stylesheet" href="styles/stylebasic.css">
+    <script src="https://kit.fontawesome.com/06b4811c66.js"></script>
+</head>
+<body>
+
+    <!-- Header --> 
+    <?php include 'header.php';  ?>
+    <!-- Menu -->
+    <?php include 'menu.php';  ?>
+    <!-- Content -->
+<img src="images/stand-optica1.jpg"   alt="stand">
+    <h1> Bienvenidos </h1>
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate cupiditate, libero nostrum esse autem voluptatem soluta nemo magni illum a alias laborum corrupti molestiae expedita. Aperiam cum eaque deserunt ratione! </p>
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate cupiditate, libero nostrum esse autem voluptatem soluta nemo magni illum a alias laborum corrupti molestiae expedita. Aperiam cum eaque deserunt ratione! </p>
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate cupiditate, libero nostrum esse autem voluptatem soluta nemo magni illum a alias laborum corrupti molestiae expedita. Aperiam cum eaque deserunt ratione! </p>
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate cupiditate, libero nostrum esse autem voluptatem soluta nemo magni illum a alias laborum corrupti molestiae expedita. Aperiam cum eaque deserunt ratione! </p>
+    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate cupiditate, libero nostrum esse autem voluptatem soluta nemo magni illum a alias laborum corrupti molestiae expedita. Aperiam cum eaque deserunt ratione! </p>
+    
+    <!-- Footer --> 
+    <?php include 'footer.php';  ?>
+    <br><br><p>ENVIAME MENSAJE
+    <br><a href="https://api.whatsapp.com/send/?phone=59169622403&text&app_absent=0" 
+    target="_blank"><i class="fab fa-whatsapp-square" style="font-size:70px;"></i></a>
+    </p>
+</body>
+</html>
